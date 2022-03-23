4472ae38b19bb195591c352365b0b24530f3ca8b
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-23 12:31:55 -0700

    commenting out unused vars and imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import App from './App';
 
 it('renders a welcome message when signed out', async () => {
   // const { container } = render(
