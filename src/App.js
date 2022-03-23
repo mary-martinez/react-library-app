af0ee47a270c8519fd37ea0619e4a15f8f3b505b
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-23 12:00:55 -0700

    books displaying and book details displaying on manual naviation to the id address

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import BookList from './views/Books/BookList';
+import BookDetail from './views/Books/BookDetail';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './views/Main/Main';
+import Header from './components/header/Header';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <BrowserRouter>
+      <Header />
+      <div className='App'>
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/books/:id'>
+            <BookDetail />
+          </Route>
+          <Route path='/books'>
+            <BookList />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+    // <main className="container">
+    //   <h1>Library Catalog</h1>
+    //   <BookList />
+    // </main>
   );
 }
 
---
+++ src/views/Main/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>Checkout everything the library has to offer! Click on Books to learn more</div>
+  );
+}
